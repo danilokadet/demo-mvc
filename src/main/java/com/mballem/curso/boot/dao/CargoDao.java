173637d1a0ca85cc1d051fdc28dff50fa3debef5
danilokadet <daniloks2@gmail.com>
2021-05-13 09:51:57 -0300

    Paginator Cargos

--- src/main/java/com/mballem/curso/boot/dao/CargoDao.java
+++ src/main/java/com/mballem/curso/boot/dao/CargoDao.java
@@ -3,6 +3,7 @@ package com.mballem.curso.boot.dao;
 import java.util.List;
 
 import com.mballem.curso.boot.domain.Cargo;
+import com.mballem.curso.boot.util.PaginacaoUtil;
 
 public interface CargoDao {
 
@@ -15,4 +16,6 @@ public interface CargoDao {
     Cargo findById(Long id);
 
     List<Cargo> findAll();
+    
+    PaginacaoUtil<Cargo> buscaPaginada(int pagina, String direcao);
 }
--- src/main/java/com/mballem/curso/boot/web/validator/FuncionarioValidator.java
+++ src/main/java/com/mballem/curso/boot/web/validator/FuncionarioValidator.java
@@ -11,18 +11,19 @@ public class FuncionarioValidator implements Validator {
 
 	@Override
 	public boolean supports(Class<?> clazz) {
-		
+		System.out.println("supports: " + Funcionario.class.equals(clazz));
 		return Funcionario.class.equals(clazz);
 	}
 
 	@Override
 	public void validate(Object object, Errors errors) {
+		System.out.println("validate: true");
 		
 		Funcionario f = (Funcionario) object;
 		
 		LocalDate entrada = f.getDataEntrada();
 		
-		if (f.getDataSaida() != null) {
+		if (f.getDataEntrada() != null && f.getDataSaida() != null) {
 			if (f.getDataSaida().isBefore(entrada)) {
 				errors.rejectValue("dataSaida", "PosteriorDataEntrada.funcionario.dataSaida");
 			}
